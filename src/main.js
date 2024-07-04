705b4ee12604ac8fa9bc290a082ddb32c1c2295f
Matteo Donini <167872740+mattdonini@users.noreply.github.com>
2024-07-04 10:15:18 +0200

    main.js

--- src/main.js
+++ src/main.js
@@ -433,9 +433,9 @@ document.querySelectorAll('[data-garment-id]').forEach((element) => {
 
                 pixelationPass.uniforms.pixelSize.value = 0.008 * easedProgress;
                 noisePass.uniforms.noiseStrength.value = 0.5 * easedProgress;
-                glitchPass.uniforms.uAmount.value = 10 * easedProgress;
+                glitchPass.uniforms.uAmount.value = 100 * easedProgress;
                 glitchPass.uniforms.uChromAbb.value = 3 * easedProgress;
-                glitchPass.uniforms.uGlitch.value = 10 * easedProgress;
+                glitchPass.uniforms.uGlitch.value = 100 * easedProgress;
 
                 if (progress < 1) {
                     requestAnimationFrame(transitionOut);
